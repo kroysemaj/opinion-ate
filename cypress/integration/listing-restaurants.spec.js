3d33197f4266b0b3b93d8520cd48ed295431f0f4
James York <kroysemaj@gmail.com>
2021-11-03 15:47:18 -0400

    Added integration test for /restaurants

---
+++ cypress/integration/listing-restaurants.spec.js
@@ -0,0 +1,22 @@
+const API_KEY = "v4JijKvrA2tZaATyJEKFIGZSFONwCcb4";
+
+describe("Listing Restaurants", () => {
+  it("shows restaurants from the server", () => {
+    const sushiPlace = "Sushi Place";
+    const pizzaPlace = "Pizza Place";
+
+    cy.server({force404: true});
+    cy.route({
+      method: "GET",
+      url: `http://outside-in-dev-api.herokuapp.com/${API_KEY}/restaurants`,
+      response: [
+        {id: 1, name: sushiPlace},
+        {id: 1, name: pizzaPlace},
+      ],
+    });
+
+    cy.visit("/");
+    cy.contains(sushiPlace);
+    cy.contains(pizzaPlace);
+  });
+});
