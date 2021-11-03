521586457e5a359226e4836b81302aa90b3a62ac
James York <kroysemaj@gmail.com>
2021-11-03 17:47:11 -0400

    fix a typo

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,11 @@
 import {createStore, applyMiddleware, compose} from "redux";
 import thunk from "redux-thunk";
-import {devToolEnhancer} from "redux-devtools-extension";
+import {devToolsEnhancer} from "redux-devtools-extension";
 import rootReducer from "./reducers";
 
 const store = createStore(
   rootReducer,
-  compose(applyMiddleware(thunk), devToolEnhancer()),
+  compose(applyMiddleware(thunk), devToolsEnhancer()),
 );
 
 export default store;
