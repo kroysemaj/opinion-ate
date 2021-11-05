9cfda76fd33f6f6286d0a1c139dec291622ce6fa
James York <kroysemaj@gmail.com>
2021-11-04 21:54:31 -0400

    styled the app

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,4 +1,7 @@
 import {useEffect} from "react";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
 import {connect} from "react-redux";
 import {loadRestaurants} from "../store/restaurants/actions";
 
@@ -7,11 +10,13 @@ export const RestaurantList = ({loadRestaurants, restaurants}) => {
     loadRestaurants();
   }, [loadRestaurants]);
   return (
-    <ul>
+    <List>
       {restaurants.map(restaurant => (
-        <li key={restaurant.id}>{restaurant.name}</li>
+        <ListItem key={restaurant.id}>
+          <ListItemText>{restaurant.name}</ListItemText>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
